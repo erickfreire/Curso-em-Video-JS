a8c654c7f54fedc5ad2060b639ce53bbc117ff06
Erick <erickfsbmantel@outlook.com>
2024-11-22 23:57:37 -0300

    at2

---
+++ Aula 12ex/ex014/script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Dependendo da hora atual do sistema, o site vai mostra que está de tarde, noite ou de dia além de mostrar a hora atual vai trocar a img e o tema dependendo da parte do dia*/
