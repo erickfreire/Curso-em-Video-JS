c4caa9400309ee7ba03272df215eae4b5c2c0289
Erick <erickfsbmantel@outlook.com>
2024-12-09 02:01:23 -0300

    commit 4

--- Aula 11/ex008.js
+++ Aula 11/ex008.js
@@ -1,2 +1,14 @@
-var vel = 60.5
+var vel = 60
 console.log(`A velocidade é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}else{
+    console.log(`Dirigia sempre usando cinto de segurança!`)
+}
+
+
+
+
+
+
+//15:20  Condições (Parte 1) - Curso JavaScript #11
